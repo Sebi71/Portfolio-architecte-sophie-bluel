bdbf74e0d041eab8458b84e009170b313bea7320
Sébastien <seb.juillet987@hotmail.fr>
2024-01-25 17:37:13 +0100

    finalisation de la partie filtre et couleur

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -5,6 +5,7 @@
 let works = [];
 let categories = [];
 
+/** Access to gallery element*/
 const galleryElement = document.querySelector(".gallery");
 
 
@@ -48,59 +49,91 @@ fetch("http://localhost:5678/api/categories")
         /**GALLLERY */
 
 /**Function DELETE old image HTML */
-function deleteOldGallery (){
+function deleteWorks (){
     const oldGallery = document.querySelector(".gallery");
     while (oldGallery.firstChild){
         oldGallery.removeChild(oldGallery.firstChild)
     }
 }
-deleteOldGallery()
 
 
-/**Function CREATE new images API*/
-function createGallery(categoryID = null){
-    // console.log(works);
-    works.forEach(item => {
-        
-        const figure = document.createElement("figure");
-        
-        const imageElement = document.createElement("img")
-        imageElement.src = item.imageUrl;
-        imageElement.setAttribute("alt", item.title)
+/**Function CREATE new works API*/
+function createGallery(categories = null){
+    /**Delete gallery(works) */
+    deleteWorks()
 
-        const titleImage = document.createElement("figcaption");
-        titleImage.innerText = item.title;
+    
+    
+    /**Looping on each work */
+    works.forEach(work => {
+            /**Creation of work item */
+            const figure = document.createElement("figure");
+            
+            const imageElement = document.createElement("img");
+            imageElement.src = work.imageUrl;
+            imageElement.setAttribute("alt", work.title);
+
+            const titleImage = document.createElement("figcaption");
+            titleImage.innerText = work.title;
+            
+            galleryElement.appendChild(figure);
+            figure.appendChild(imageElement);
+            figure.appendChild(titleImage);
         
-        galleryElement.appendChild(figure);
-        figure.appendChild(imageElement);
-        figure.appendChild(titleImage);
 
     });
-}
+};
 
 
-    /**FILTER */
-    
-    function createFilter(){
+        /**FILTER */
+/**Adding filters of categories to filter work in the gallery */    
+function createFilter(){
+    /**Creating a new object in the table*/
+    categories.unshift({id: 0, name: "Tous"});
+
+    /**Creating div element for categories */
+    const portefolio = document.getElementById("portfolio");
+    const categoriesElement = document.createElement("div");
+    categoriesElement.classList.add("categories");
+    portefolio.insertBefore(categoriesElement, galleryElement);
+
+    /**Looping on each category */
+    categories.forEach((categoryElement, i) => {
+        /**Creation of buttons for different categories */
+        const categoryBtn = document.createElement("button");
+        categoryBtn.innerText = categoryElement.name;
+        categoryBtn.value = categoryElement.id;
+        categoryBtn.classList.add("category-btn");
+        if(i === 0){
+            categoryBtn.classList.add("category-selected")
+        };
+        categoriesElement.appendChild(categoryBtn);
         
-        const portefolio = document.getElementById("portfolio");
-        const categoriesElement = document.createElement("div");
-        categoriesElement.classList.add("categories")
-        portefolio.insertBefore(categoriesElement, galleryElement);
-        
-        categories.unshift({id: 0, name: "Tous"});
-        
-        categories.forEach(item => {
-            const categoryBtn = document.createElement("button");
-            categoryBtn.classList.add("category-btn");
-            categoryBtn.innerText = item.name;
-            categoryBtn.value = item.id;
-            categoriesElement.appendChild(categoryBtn);
+        /**Change catégory witch function addEventListener click */
+        categoryBtn.addEventListener("click", (e) => {
+            const selectedCategoryId = parseInt(e.target.value);
+
+            let filterWorks = works;
+            if (selectedCategoryId !== 0) {
+                filterWorks = works.filter(work => {
+                return work.categoryId === selectedCategoryId;
+                });
+            } 
+            createGallery();
+            console.log(filterWorks);
             
-            categoryBtn.addEventListener("click", (e) => {
-                console.log("Bouton de catégorie cliqué", e.target.value);
-        })
-        
-    })  
-    console.log(categories);
-}  
+            /**change color button */
+            const filterColorCategory = document.querySelectorAll(".category-btn");
+            filterColorCategory.forEach((filterColor, i) => {
+                if(i === selectedCategoryId){
+                    filterColor.classList.add("category-selected")
+                } else {
+                    if(filterColor.classList.contains("category-selected")){
+                        filterColor.classList.remove("category-selected")
+                    };
+                };
+            });
+
+        });
+    });  
+};  
